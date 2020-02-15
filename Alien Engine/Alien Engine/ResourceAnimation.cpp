a230a8321df979206e707e4515712f78de133905
YessicaSD <36170847+YessicaSD@users.noreply.github.com>
2020-02-15 19:21:25 +0100

    adding resourceAnimation

---
+++ Alien Engine/Alien Engine/ResourceAnimation.cpp
@@ -0,0 +1,54 @@
+#include "Application.h"
+#include "ModuleImporter.h"
+#include "ResourceAnimation.h"
+
+
+ResourceAnimation::ResourceAnimation(uint id) : Resource()
+{
+}
+
+ResourceAnimation::~ResourceAnimation()
+{
+}
+
+
+std::string ResourceAnimation::GetTypeString() const
+{
+	return "Animation";
+}
+
+float ResourceAnimation::GetDuration() const
+{
+	return ((float)tick_duration / (float)ticks_per_second);
+}
+
+uint ResourceAnimation::GetChannelIndex(std::string name)
+{
+	uint i;
+	for (i = 0; i < num_channels; i++) {
+		if (channels[i].name == name) {
+			return i;
+		}
+	}
+
+	return num_channels;
+}
+
+void ResourceAnimation::UnLoad()
+{
+	LOG_ENGINE("Unloading Animation %s", name.c_str());
+
+	for (int i = 0; i < num_channels; i++)
+	{
+		delete[] channels[i].position_keys;
+		delete[] channels[i].scale_keys;
+		delete[] channels[i].rotation_keys;
+	}
+
+	channels = nullptr;
+}
+
+void ResourceAnimation::Load()
+{
+	//App->importer->animation->Load(this);
+}
---
+++ Alien Engine/Alien Engine/ResourceAnimation.h
@@ -0,0 +1,57 @@
+#pragma once
+#ifndef _RESOURCE_ANIMATION_H_
+#define _RESOURCE_ANIMATION_H_
+
+#include "Resource.h"
+#include <map>
+template<typename TYPE>
+struct KeyAnimation
+{
+	double time;
+	TYPE value;
+};
+
+class ResourceAnimation : public Resource
+{
+public:
+	struct Channel {
+		std::string name = "";
+		uint num_position_keys = 0;
+		uint num_scale_keys = 0;
+		uint num_rotation_keys = 0;
+
+		KeyAnimation<float3>* position_keys;
+		KeyAnimation<float3>* scale_keys;
+		KeyAnimation<Quat>* rotation_keys;
+	};
+
+public:
+
+	ResourceAnimation(uint id);
+	~ResourceAnimation();
+
+	std::string GetTypeString() const;
+	float GetDuration() const;
+
+	uint GetChannelIndex(std::string name);
+
+	void UnLoad();
+	void Load();
+
+public:
+	bool loops = false;
+	std::string name = "";
+	uint tick_duration = 0;
+	uint ticks_per_second = 0;
+	uint start_tick = 0;
+	uint end_tick = 0;
+	uint max_tick = 0;
+
+	uint num_channels = 0;
+	Channel* channels;
+};
+
+
+#endif // !_RESOURCE_ANIMATION_H_
+
+
