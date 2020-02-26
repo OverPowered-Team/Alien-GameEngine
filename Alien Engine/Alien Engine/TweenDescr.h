75a2f2c39bfe3bfaeec7f9d6db3c5c88f0f92a6a
Alex Morales Garcia <36154498+AlexMG99@users.noreply.github.com>
2020-02-26 11:40:50 +0100

    Create class UITween and TweenDescr

---
+++ Alien Engine/Alien Engine/TweenDescr.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "UITween.h"
+#include "MathGeoLib/include/Math/float3.h"
+
+class TweenDescr {
+public:
+	float3 from;
+	float3 to;
+	float time;
+	TweenType type;
+
+
+
+};
---
+++ Alien Engine/Alien Engine/UITween.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "Module.h"
+
+class TweenDescr;
+
+enum TweenType {
+	MOVE_X,
+	MOVE_Y,
+	MOVE_X,
+	MOVE_LOCAL_X,
+	MOVE_LOCAL_Y,
+	MOVE_LOCAL_Z,
+	ROTATE_X,
+	ROTATE_Y,
+	ROTATE_Z,
+	SCALE_X,
+	SCALE_Y,
+	SCALE_Z
+};
+
+enum UITweenType {
+	notUsed, linear
+};
+
+class UITween: Module
+{
+public:
+	UITween() {};
+	~UITween() {};
+
+public:
+	static int maxTweens;
+
+private:
+	static TweenDescr* tweens;
+};
