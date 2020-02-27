17011bb3ee84cefb1db6ea1c0f5a43bedaa23a84
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2020-02-27 09:36:10 +0100

    last fix

--- Alien Engine/Alien Engine/ReturnZ.cpp
+++ Alien Engine/Alien Engine/ReturnZ.cpp
@@ -489,7 +489,7 @@ void ReturnZ::CreateObject(ActionDeleteObject* obj)
 			std::vector<CompZ*>::iterator item = obj->object->comps.begin();
 			for (; item != obj->object->comps.end(); ++item) {
 				if (*item != nullptr) {
-					SDL_assert((uint)ComponentType::UNKNOWN == 4); // add new type to switch
+					SDL_assert((uint)ComponentType::UNKNOWN == 26); // add new type to switch
 					switch ((*item)->type)
 					{
 					case ComponentType::TRANSFORM: {
