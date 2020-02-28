73e84b6bda8d27aa9d20eda31c965d69dc1ce3a7
Alex Morales Garcia <36154498+AlexMG99@users.noreply.github.com>
2020-02-28 10:10:12 +0100

    Lel

--- Alien Engine/Alien Engine/AnimTween.cpp
+++ Alien Engine/Alien Engine/AnimTween.cpp
@@ -9,7 +9,6 @@ Tween* AnimTween::Move(GameObject * gameObject, const float3 & to, float time)
 	Tween* tween = nullptr;
 
 	tween->to = float4(to, 0.f);
-	tween->trans = gameObject;
 
 	return tween;
 }
