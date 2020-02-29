eb97eb3183385e248fe03e76fb205a79b7fcc639
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2020-02-29 21:52:34 +0100

    testing animators

--- Alien Engine/CoreData/AlienEngineScripts/PlayerController.cpp
+++ Alien Engine/CoreData/AlienEngineScripts/PlayerController.cpp
@@ -10,8 +10,18 @@ PlayerController::~PlayerController()
 
 void PlayerController::Start()
 {
+	animator = (ComponentAnimator*)GetComponent(ComponentType::ANIMATOR);
 }
 
 void PlayerController::Update()
 {
+	playerData.speed = 0;
+
+	if (Input::GetKeyRepeat(SDL_SCANCODE_W)) {
+		playerData.speed += (100 * Time::GetDT());
+	}
+
+	transform->SetLocalPosition({ transform->GetLocalPosition().x, transform->GetLocalPosition().y, transform->GetLocalPosition().z + playerData.speed });
+	animator->SetFloat("speed", playerData.speed);
+	animator->SetBool("Attcking", false);
 }
