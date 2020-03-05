9cda4e3469a826a9db39b05942d17b6a3a7c90f3
Christian Martínez <christt105@gmail.com>
2020-03-05 16:54:09 +0100

    merge

--- Alien Engine/Alien Engine/ComponentUI.cpp
+++ Alien Engine/Alien Engine/ComponentUI.cpp
@@ -276,7 +276,7 @@ void ComponentUI::CheckFirstSelected()
 {
 	if (tabbable && this->game_object_attached != nullptr && this->game_object_attached->enabled)
 	{
-		if (App->objects->selected_ui != -1)
+		if (App->objects->GetGameObjectByID(App->objects->selected_ui)!=nullptr &&  App->objects->selected_ui != -1)
 			App->objects->GetGameObjectByID(App->objects->selected_ui)->GetComponent<ComponentUI>()->state = Release;
 
 		App->objects->selected_ui = this->game_object_attached->ID;
