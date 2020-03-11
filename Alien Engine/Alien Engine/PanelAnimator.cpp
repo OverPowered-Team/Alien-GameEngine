9502d68c0201604a816e2d70096a6f42a94016aa
CereVenteo <36203330+CereVenteo@users.noreply.github.com>
2020-03-11 22:27:15 +0100

    bugfix when close engine

--- Alien Engine/Alien Engine/PanelAnimator.cpp
+++ Alien Engine/Alien Engine/PanelAnimator.cpp
@@ -656,7 +656,31 @@ void PanelAnimator::OnPlay()
 
 void PanelAnimator::OnStop()
 {
-	OnObjectSelect();
+	if (App->objects->GetSelectedObjects().size() == 1)
+	{
+		if (App->objects->GetSelectedObjects().back()->HasComponent(ComponentType::ANIMATOR)) {
+			ComponentAnimator* c_anim = (ComponentAnimator*)App->objects->GetSelectedObjects().back()->GetComponent(ComponentType::ANIMATOR);
+			ResourceAnimatorController* anim_ctrl = nullptr;
+
+			current_animator = c_anim->GetResourceAnimatorController();
+		}
+		else
+			current_animator = nullptr;
+	}
+	else if (App->objects->GetSelectedObjects().size() > 1) {
+		for each (GameObject * go in App->objects->GetSelectedObjects())
+		{
+			if (go->HasComponent(ComponentType::ANIMATOR)) {
+				ComponentAnimator* c_anim = (ComponentAnimator*)go->GetComponent(ComponentType::ANIMATOR);
+				ResourceAnimatorController* anim_ctrl = nullptr;
+
+				current_animator = c_anim->GetResourceAnimatorController();
+				break;
+			}
+			else
+				current_animator = nullptr;
+		}
+	}
 }
 
 void PanelAnimator::SetCurrentResourceAnimatorController(ResourceAnimatorController* animator)
