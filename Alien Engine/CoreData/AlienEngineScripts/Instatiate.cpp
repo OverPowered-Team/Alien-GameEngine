66f6a50d8e024941b6ba293302631715b1c976f1
Alex Morales Garcia <36154498+AlexMG99@users.noreply.github.com>
2020-04-02 16:58:32 +0200

    Solved physics children rb and char controller ijnstatiation

--- Alien Engine/CoreData/AlienEngineScripts/Instatiate.cpp
+++ Alien Engine/CoreData/AlienEngineScripts/Instatiate.cpp
@@ -16,6 +16,6 @@ void Instatiate::Update()
 {
 	if (Input::GetKeyDown(SDL_SCANCODE_I))
 	{
-		GameObject::Instantiate(prefab, float3(20, 20, 20));
+		GameObject::Instantiate(prefab, float3(20, 20, 20), true);
 	}
 }
