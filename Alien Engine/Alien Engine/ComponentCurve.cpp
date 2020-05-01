a69edb30548635ff3436b9f9e9614843d05aa673
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2020-05-01 17:28:16 +0200

    clean

--- Alien Engine/Alien Engine/ComponentCurve.cpp
+++ Alien Engine/Alien Engine/ComponentCurve.cpp
@@ -3,40 +3,14 @@
 #include "ReturnZ.h"
 #include "Maths.h"
 #include "glew/include/glew.h"
-#include "Bezier/tinysplinecxx.h"
 #include "Application.h"
 #include "ModuleInput.h"
 
-static 	tinyspline::BSpline spline(5, 3);
-static tinyspline::BSpline beizer;
 ComponentCurve::ComponentCurve(GameObject* attach) : Component(attach)
 {
 	type = ComponentType::CURVE;
 
-	std::vector<tinyspline::real> controlPoints = spline.controlPoints();
-	controlPoints[0] = -10;
-	controlPoints[1] = 0;
-	controlPoints[2] = 10;
-
-	controlPoints[3] = 0;
-	controlPoints[4] = 10;
-	controlPoints[5] = -10;
-
-	controlPoints[6] = 10;
-	controlPoints[7] = 0;
-	controlPoints[8] = 30;
-
-	controlPoints[9] = 20;
-	controlPoints[10] = 20;
-	controlPoints[11] = 4;
-
-	controlPoints[12] = 40;
-	controlPoints[13] = -20;
-	controlPoints[14] = -3;
-
-	spline.setControlPoints(controlPoints);
-
-	beizer = spline.toBeziers();
+	CreateBaseCurve();
 }
 
 ComponentCurve::~ComponentCurve()
@@ -94,69 +68,33 @@ void ComponentCurve::LoadComponent(JSONArraypack* to_load)
 {
 }
 
+void ComponentCurve::CreateBaseCurve()
+{
+	beizer = tinyspline::BSpline(4, 3, 3, tsBSplineType::TS_BEZIERS);
+
+	std::vector<tinyspline::real> control_points = beizer.controlPoints();
+
+	control_points[0] = -10;
+	control_points[1] = 0;
+	control_points[2] = 0;
+
+	control_points[3] = -5;
+	control_points[4] = 0;
+	control_points[5] = 0;
+
+	control_points[6] = 0;
+	control_points[7] = 0;
+	control_points[8] = 0;
+
+	control_points[9] = 5;
+	control_points[10] = 0;
+	control_points[11] = 0;
+
+	beizer.setControlPoints(control_points);
+}
+
 void ComponentCurve::DrawScene()
 {	
-	if (App->input->GetKey(SDL_SCANCODE_1) == KEY_DOWN) {
-		spline = tinyspline::BSpline(6,3);
-		std::vector<tinyspline::real> controlPoints = spline.controlPoints();
-		controlPoints[0] = -10;
-		controlPoints[1] = 0;
-		controlPoints[2] = 10;
-
-		controlPoints[3] = 0;
-		controlPoints[4] = 10;
-		controlPoints[5] = -10;
-
-		controlPoints[6] = 10;
-		controlPoints[7] = 0;
-		controlPoints[8] = 30;
-
-		controlPoints[9] = 20;
-		controlPoints[10] = 20;
-		controlPoints[11] = 4;
-
-		controlPoints[12] = 40;
-		controlPoints[13] = -20;
-		controlPoints[14] = -3;
-
-		controlPoints[12] = 100;
-		controlPoints[13] = 160;
-		controlPoints[14] = 120;
-
-		spline.setControlPoints(controlPoints);
-
-		beizer = spline.toBeziers();
-
-	}
-
-	if (App->input->GetKey(SDL_SCANCODE_2) == KEY_DOWN) {
-		spline = tinyspline::BSpline(4, 3);
-		std::vector<tinyspline::real> controlPoints = spline.controlPoints();
-		controlPoints[0] = -10;
-		controlPoints[1] = 0;
-		controlPoints[2] = 10;
-
-		controlPoints[3] = 0;
-		controlPoints[4] = 10;
-		controlPoints[5] = -10;
-
-		controlPoints[6] = 10;
-		controlPoints[7] = 0;
-		controlPoints[8] = 30;
-
-		controlPoints[9] = 20;
-		controlPoints[10] = 20;
-		controlPoints[11] = 4;
-
-		spline.setControlPoints(controlPoints);
-
-		beizer = spline.toBeziers();
-	}
-
-	if (App->input->GetKey(SDL_SCANCODE_3) == KEY_DOWN) {
-		beizer.setControlPointAt(1, { -50, 10, 20 });
-	}
-
 	glPointSize(9);
 	glBegin(GL_POINTS);
 	for (uint i = 0; i < beizer.controlPoints().size(); i += 3) {
@@ -170,46 +108,4 @@ void ComponentCurve::DrawScene()
 		glVertex3f(res[0], res[1], res[2]);
 	}
 	glEnd();
-
-}
-
-float3 Curve::ValueAt(float at)
-{
-	if (at < 0 || at > 1 || curve_points.empty() || control_points.empty()) {
-		return float3::zero();
-	}
-
-	uint index = Maths::Map(at, 0, 1, 0, curve_points.size() - 1);
-	return curve_points[index];
-}
-
-void Curve::CreatePoints(float detail, const float3& p0, const float3& p1, const float3& p2, const float3& p3)
-{
-	float3 res = float3::zero();
-
-	control_points.clear();
-	curve_points.clear();
-
-	control_points.push_back(p0);
-	control_points.push_back(p1);
-	control_points.push_back(p2);
-	control_points.push_back(p3);
-
-	for (float t = 0; t < 1; t += detail) {
-		curve_points.push_back(bezier(t, (float*)p0.ptr(), (float*)p1.ptr(), (float*)p2.ptr(), (float*)p3.ptr()));
-	}
-}
-
-const std::vector<float3>& Curve::GetControlPoints()
-{
-	return control_points;
-}
-
-float3 Curve::bezier(float t, float* p0, float* p1, float* p2, float* p3)
-{
-	float3 res = float3::zero();
-	res[0] = pow((1 - t), 3) * p0[0] + 3 * t * pow((1 - t), 2) * p1[0] + 3 * pow(t, 2) * (1 - t) * p2[0] + pow(t, 3) * p3[0];
-	res[1] = pow((1 - t), 3) * p0[1] + 3 * t * pow((1 - t), 2) * p1[1] + 3 * pow(t, 2) * (1 - t) * p2[1] + pow(t, 3) * p3[1];
-	res[2] = pow((1 - t), 3) * p0[2] + 3 * t * pow((1 - t), 2) * p1[2] + 3 * pow(t, 2) * (1 - t) * p2[2] + pow(t, 3) * p3[2];
-	return res;
 }
--- Alien Engine/Alien Engine/ComponentCurve.h
+++ Alien Engine/Alien Engine/ComponentCurve.h
@@ -1,31 +1,8 @@
 #pragma once
 
 #include "Component.h"
-#include "MathGeoLib/include/Math/float3.h"
 #include <vector>
-
-class __declspec(dllexport) Curve {
-public:
-	Curve() {}
-	~Curve() {}
-
-	// 0 - 1
-	float3 ValueAt(float at);
-
-	void CreatePoints(float detail, const float3& p0, const float3& p1, const float3& p2, const float3& p3);
-
-	const std::vector<float3>& GetControlPoints();
-
-private:
-
-	float3 bezier(float t, float* p0, float* p1, float* p2, float* p3);
-
-private:
-
-	std::vector<float3> control_points;
-	std::vector<float3> curve_points;
-
-};
+#include "Bezier/tinysplinecxx.h"
 
 class __declspec(dllexport) ComponentCurve : public Component {
 public:
@@ -44,9 +21,11 @@ private:
 	void SaveComponent(JSONArraypack* to_save);
 	void LoadComponent(JSONArraypack* to_load);
 
+	void CreateBaseCurve();
+
 public:
 
-	Curve curve;
+	tinyspline::BSpline beizer;
 
 };
 
