ead9f88e6486cfa178a742d2fb8bec9f11715222
CarlosPeña <36167348+CarlosUPC@users.noreply.github.com>
2020-05-06 11:14:26 +0200

    testing this lib

--- Alien Engine/Alien Engine/Globals.h
+++ Alien Engine/Alien Engine/Globals.h
@@ -19,7 +19,7 @@ __declspec(dllexport) void log(const char file[], int line, const char* format, 
 
 
 // DISCOMMENT TO START THE ENGINE IN PLAY MODE
-//crec#define GAME_VERSION
+//#define GAME_VERSION
 
 #define RELEASE( x )\
     {\
