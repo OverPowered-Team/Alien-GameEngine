6fae39767f43d0ca10ebb929b6b1b27b50f9a750
Pol <36154612+PolRecasensSarra@users.noreply.github.com>
2020-05-24 12:30:45 +0200

    solved bug Canvas not rendering if there was a canvas world and a normal canvas

--- Alien Engine/Alien Engine/ModuleObjects.cpp
+++ Alien Engine/Alien Engine/ModuleObjects.cpp
@@ -449,17 +449,6 @@ update_status ModuleObjects::PostUpdate(float dt)
 			
 			ComponentCamera* mainCamera = App->renderer3D->GetCurrentMainCamera();
 
-			std::vector<std::pair<float, GameObject*>>::iterator it_ui_2d = ui_2d.begin();
-			for (; it_ui_2d != ui_2d.end(); ++it_ui_2d) {
-				if ((*it_ui_2d).second != nullptr) {
-					ComponentUI* ui = (*it_ui_2d).second->GetComponent<ComponentUI>();
-					if (ui != nullptr && ui->IsEnabled())
-					{
-						ui->Draw(!printing_scene);
-
-					}
-				}
-			}
 			std::vector<std::pair<float, GameObject*>>::iterator it_ui_world = ui_world.begin();
 			for (; it_ui_world != ui_world.end(); ++it_ui_world) {
 				if ((*it_ui_world).second != nullptr) {
@@ -473,6 +462,19 @@ update_status ModuleObjects::PostUpdate(float dt)
 					}
 				}
 			}
+
+			std::vector<std::pair<float, GameObject*>>::iterator it_ui_2d = ui_2d.begin();
+			for (; it_ui_2d != ui_2d.end(); ++it_ui_2d) {
+				if ((*it_ui_2d).second != nullptr) {
+					ComponentUI* ui = (*it_ui_2d).second->GetComponent<ComponentUI>();
+					if (ui != nullptr && ui->IsEnabled())
+					{
+						ui->Draw(!printing_scene);
+
+					}
+				}
+			}
+			
 
 			if (printing_scene)
 				OnDrawGizmos();
