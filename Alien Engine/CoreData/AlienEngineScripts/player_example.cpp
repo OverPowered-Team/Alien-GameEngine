a5e04ff896e873851c012c6e42a3b1e99b0a21d4
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2020-05-25 14:33:40 +0200

    release in engine

--- Alien Engine/CoreData/AlienEngineScripts/player_example.cpp
+++ Alien Engine/CoreData/AlienEngineScripts/player_example.cpp
@@ -1,4 +1,5 @@
 #include "player_example.h"
+#include "..\..\ComponentBar.h"
 
 player_example::player_example() : Alien()
 {
@@ -8,11 +9,24 @@ player_example::~player_example()
 {
 }
 
-
-
 void player_example::Update()
 {
-	if (Input::GetKeyDown(SDL_SCANCODE_3)) {
-		GameObject::Instantiate(p, { 0,0,0 }, false, GameObject::FindWithName("Canvas"));
+	if (Input::GetKeyRepeat(SDL_SCANCODE_3)) {
+		ComponentBar* bar = GetComponent<ComponentBar>();
+		float value = bar->GetBarValue();
+		value += Time::GetDT();
+		if (value > 100) {
+			value = 100;
+		}
+		bar->SetBarValue(value);
+	}
+	if (Input::GetKeyRepeat(SDL_SCANCODE_4)) {
+		ComponentBar* bar = GetComponent<ComponentBar>();
+		float value = bar->GetBarValue();
+		value -= Time::GetDT();
+		if (value < 0) {
+			value = 0;
+		}
+		bar->SetBarValue(value);
 	}
 }
