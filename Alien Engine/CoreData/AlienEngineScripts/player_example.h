7d54b2c4388dd12510c5a82b5455f25d7a8da037
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2020-05-25 14:15:00 +0200

    Fixing component bar (2 of 3 types now works) working on the last one

--- Alien Engine/CoreData/AlienEngineScripts/player_example.h
+++ Alien Engine/CoreData/AlienEngineScripts/player_example.h
@@ -10,46 +10,18 @@ public:
 	player_example();
 	virtual ~player_example();
 
-	void Start();
 	void Update();
 
-	// only performed when Move (controller) is called
-	void OnControllerColliderHit(ControllerColliderHit hit);
-
-
-
-
-	// COLLISION AND TRIGGERS
-	void OnCollisionEnter(const Collision& collision);
-	void OnCollisionStay(const Collision& collision);
-	void OnCollisionExit(const Collision& collision);
-
-
-	void OnTriggerEnter(ComponentCollider* collider);
-
-	// etc
-
-	void TEst(){}
-
-
 public:
-	ComponentCharacterController* ccontroller = nullptr;
-
-	float speed = 10.0f;
-	float jumpSpeed = 10.0f;
-	float gravity = 20.0f;
-
-	float3 moveDirection;
-
-	GameObject* obj = nullptr;
+	Prefab p;
 };
 
 
 ALIEN_FACTORY player_example* Createplayer_example() {
 	player_example* alien = new player_example();
 	// To show in inspector here
-	SHOW_VOID_FUNCTION(player_example::TEst, alien);
 
-	SHOW_IN_INSPECTOR_AS_GAMEOBJECT(alien->obj);
+
+	SHOW_IN_INSPECTOR_AS_PREFAB(alien->p);
 	return alien;
 } 
