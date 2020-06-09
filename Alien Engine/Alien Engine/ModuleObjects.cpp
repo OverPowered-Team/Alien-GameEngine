046c680b626e9ee36874bc8ef5af8e1689df3534
Yessica Servin Dominguez <36170847+YessicaSD@users.noreply.github.com>
2020-06-09 20:41:27 +0200

    changing resource Material to export to dll and framebuffer

--- Alien Engine/Alien Engine/ModuleObjects.cpp
+++ Alien Engine/Alien Engine/ModuleObjects.cpp
@@ -498,14 +498,16 @@ update_status ModuleObjects::PostUpdate(float dt)
 				}
 			}
 			
-			glBindFramebuffer(GL_FRAMEBUFFER, 0);
-			glDisable(GL_DEPTH_TEST);
+			
 
 			if (printing_scene)
 				OnDrawGizmos();
 			if (isGameCamera) {
 				OnPostRender(viewport->GetCamera());
 			}
+			glBindFramebuffer(GL_FRAMEBUFFER, 0);
+			glDisable(GL_DEPTH_TEST);
+
 		}
 
 		// And finally draw all into the final PostProcFBO's Texture
